3f83b52dcc718f56cfc182ba8e3aff2d6bc7c187
Emmett Verba <emmettverba@gmail.com>
2024-01-29 15:55:22 -0700

    creates model for a restaraunt post and comment schema

---
+++ models/commentSchema.js
@@ -0,0 +1,18 @@
+const Schema = require('mongoose').Schema;
+
+const commentSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {
+    timestamps: true,
+    toJSON: { virtuals: true }
+});
+
+module.exports = commentSchema;
---
+++ models/restPost.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const commentSchema = require('./commentSchema')
+
+const restPost = new Schema({
+    // description, likes, location, post time, donation value, rsvp total, comments, rsvp list
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true},
+    description: {type: String},
+    location: {type: String},
+    posted: {type: Date},
+    donationValue: {type: Number},
+    rsvpTotal: {type: Number},
+    rsvpList: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    likes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    comments: [commentSchema]
+}, {
+    timestamps: true,
+    toJSON: { virtuals: true } //allows virtuals to be sent to JSON
+});
+
+module.exports = mongoose.model('RestPost', restPost);
