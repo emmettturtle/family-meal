107f9e6f46ee2d368c1be68287a3f50018c82e60
Emmett Verba <emmettverba@gmail.com>
2024-01-30 22:00:43 -0700

    Created feed instance method and subsequent findDistance method to call the google distance matrix API - not yet tested

--- models/restProfile.js
+++ models/restProfile.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const addressSchema = require('./addressSchema');
 const Schema = mongoose.Schema;
 
+const RestPost = require('./restPost');
+
 const restProfile = new Schema({
     // name, address, description, userID, signupDate, donation total
     name: {type: String, required: true},
@@ -14,7 +16,56 @@ const restProfile = new Schema({
     toJSON: { virtuals: true } //allows virtuals to be sent to JSON
 });
 
-//instance method for get feed
-//
+//instance method for get feed:
+//iterate through posts
+//get the address of this profile
+//get posts and itterate
+//get the address of the post user
+//convert both to URI
+//run API, origin is user address, destination is post address
+//if distance is less than 50 km add to feed
+restProfile.methods.feed = async function(){
+    let feed = [];
+    let userAddress = this.address.street + ', ' + this.address.city + ', ' + this.address.state + ' ' + this.address.zip;
+    const allPosts = await RestPost.find({});
+
+    allPosts.forEach(function(post){
+        let postAddress = post.profile.address;
+        let postAddressStr = postAddress.street + ', ' + postAddress.city + ', ' + postAddress.state + ' ' + postAddress.zip;
+        let distance = findDistance(userAddress, postAddressStr);
+
+        if (distance < 50000){
+            feed.push(post);
+        }
+    });
+
+    return feed;
+}
+
+//API KEY: AIzaSyCwX6U4uK-hbz7_A7ekpa9xtVeqtyqkeNo
+
+async function findDistance(pointA, pointB){
+    // URI the values
+    // run the API call
+    // return the distance in meters
+    const API_KEY = 'AIzaSyCwX6U4uK-hbz7_A7ekpa9xtVeqtyqkeNo';
+    let pointAURI = encodeURI(pointA);
+    let pointBURI = encodeURI(pointB);
+    let distance = 0;
+    try{
+        const res = await fetch(
+            `https://maps.googleapis.com/maps/api/distancematrix/json
+                ?destinations=${pointAURI}
+                &origins=${pointBURI}
+                &key=${API_KEY}`
+        );
+        const row = res.rows[0];
+        distance = row.elements[0].distance.value;
+    } catch (err) {
+        console.log(err);
+    }
+    
+    return distance;    
+}
 
 module.exports = mongoose.model('RestProfile', restProfile);
