560f529b1080a6ca557738bf0ee79dab1d33c489
Emmett Verba <emmettverba@gmail.com>
2024-02-05 10:17:17 -0700

    final touches

--- models/restProfile.js
+++ models/restProfile.js
@@ -6,7 +6,6 @@ const {Client} = require('@googlemaps/google-maps-services-js');
 const RestPost = require('./restPost');
 
 const restProfileSchema = new Schema({
-    // name, address, description, userID, signupDate, donation total
     name: {type: String, required: true},
     description: {type: String},
     totalDonations: {type: Number, default:0},
@@ -17,18 +16,7 @@ const restProfileSchema = new Schema({
     toJSON: { virtuals: true } //allows virtuals to be sent to JSON
 });
 
-restProfileSchema.statics.findProfile = function(profileId) {
 
-}
-
-//instance method for get feed:
-//iterate through posts
-//get the address of this profile
-//get posts and itterate
-//get the address of the post user
-//convert both to URI
-//run API, origin is user address, destination is post address
-//if distance is less than 50 km add to feed
 restProfileSchema.methods.feed = async function(){
     let feed = [];
     let userAddress = this.address.getAddressStr();
